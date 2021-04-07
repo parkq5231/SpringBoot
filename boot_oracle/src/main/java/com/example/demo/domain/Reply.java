d0bea6b038168209c77f5cf906fddcc38a566332
박세민 <qseft523125@naver.com>
2021-04-07 10:48:14 +0900

    JUnit/JPA

--- boot_oracle/src/main/java/com/example/demo/domain/Reply.java
+++ boot_oracle/src/main/java/com/example/demo/domain/Reply.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,7 +20,6 @@ public class Reply {
 	@Id // pk
 	@GeneratedValue // sequence를 의미
 	private Integer seq;
-	@Transient // 생성시 제외
 	private String title;
 	@Column(length = 100, name = "content")
 	private String contents;
--- boot_oracle/src/test/java/com/example/demo/ReplyRepositoryClient.java
+++ boot_oracle/src/test/java/com/example/demo/ReplyRepositoryClient.java
@@ -1,51 +1,46 @@
 package com.example.demo;
 
-//assert
 import static org.hamcrest.MatcherAssert.assertThat;
-//is
 import static org.hamcrest.Matchers.is;
 
 import java.util.List;
-
-import javax.persistence.EntityManager;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Repository;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.domain.Reply;
+import com.example.demo.repository.ReplyRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest // auto config
 @Transactional
-public class ReplyManagerClient {
+public class ReplyRepositoryClient {
 	@Autowired
-	EntityManager em;
+	ReplyRepository repository;
 
 	@Test
 	@Commit
 	public void 댓글저장() {
 		Reply reply = Reply.builder().title("kim")//
-				.contents("kimbab2").build();
-		em.persist(reply);
-		assertThat(em.find(Reply.class, reply.getSeq()), is(reply));// pk로 조회해서 담고 is로 비교
-	}
+				.contents("kimbab3").build();
+		repository.save(reply);
+	}// end of save
 
 	@Test
 	public void 댓글조회() {
-		// persist = insert
-		// merge = update
-		// remove = delete
-		// find = select
-
 		// jpql
-		List<Reply> list = em.createQuery("from Reply", Reply.class).getResultList();
+		List<Reply> list = repository.findAll();
 		System.out.println(list);
-	}
+	}// end of findAll
 
-}// end of class
+	@Test
+	public void 제목조회() {
+		List<Reply> list = repository.findByTitle("kim");
+		System.out.println(list);
+	}// end of findByTitle
+}
